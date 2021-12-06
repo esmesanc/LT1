7df63a905ad61be39c06d806affd02d1cc27c0af
Eduardo Alfredo Gallegos Cruz <natsu.eduardo.gallegos@gmail.com>
2021-12-05 22:57:53 -0600

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyARb3fUE70QrjRYvHyCoqw-mSYKQ1AzgOk",
+    authDomain: "learningtogether-faae4.firebaseapp.com",
+    projectId: "learningtogether-faae4",
+    storageBucket: "learningtogether-faae4.appspot.com",
+    messagingSenderId: "56880993418",
+    appId: "1:56880993418:web:1781c04d5e16b4baa88973",
+    measurementId: "G-XM85CRG2EV"
+  }
 };
